24705f458d16957faeb3c4c4a0c10f3491a64238
JinKu <tn400xl@naver.com>
2018-08-02 17:39:37 +0900

    0802

--- Linno/js/linno.js
+++ Linno/js/linno.js
@@ -2,5 +2,12 @@ jQuery(function($){
     $('.menu_ico_wrap').on('click',function(){
         $(this).toggleClass('nav_btn_on');
         $('.nav_wrap').toggleClass('nav_btn_on');
-    })
+    });
+  
+      
+   
+   
+    $('.slider').bxSlider();
+    
+    
 })
