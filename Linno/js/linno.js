beddf5ed517ec2adbe3be983a7e6c7bd9cb030cd
JinKu <tn400xl@naver.com>
2018-08-10 17:45:04 +0900

    0810

--- Linno/js/linno.js
+++ Linno/js/linno.js
@@ -4,7 +4,44 @@ jQuery(function($){
         $('.gnb').toggleClass('nav_btn_on');
     });
   
-      
+      $('.com_mind_btn').hover(function(){
+          $(this).toggleClass('mind_btn_action');
+      });
+    
+    $('.quality_sm_img li:last').hover(function(){
+          $('.tech2').css('opacity','0');
+         $('.tech1').css('opacity','1');
+      });
+    
+     $('.quality_sm_img li:first').hover(function(){
+          $('.tech2').css('opacity','1');
+         $('.tech1').css('opacity','0');
+      });
+    
+    $('.ov_side_menu li').click(function(){
+        $(this).addClass('ov_btn_active');
+        $('.ov_side_menu li').not(this).removeClass('ov_btn_active');
+       
+    });
+    
+    $('.ov_quality').click(function(){
+        $('.ov_quality_wrap').css('opacity','1');
+        $('.ov_content_wrap > div').not('.ov_quality_wrap').css('opacity','0');
+        $('.ov_wrap').css('padding-bottom',"70%");
+     });
+    
+      $('.ov_patent').click(function(){
+        $('.ov_patent_wrap').css('opacity','1');
+        $('.ov_content_wrap > div').not('.ov_patent_wrap').css('opacity','0');
+        $('.ov_wrap').css('padding-bottom',"20%");
+     });
+    
+    $('.ov_cert').click(function(){
+        $('.ov_cert_wrap').css('opacity','1');
+        $('.ov_content_wrap > div').not('.ov_cert_wrap').css('opacity','0');
+        $('.ov_wrap').css('padding-bottom',"70%");
+     });
+
    
    
     $('.slider').bxSlider({
