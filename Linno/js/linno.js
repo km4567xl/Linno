3bdf3968f2f59959dff5aa24772fc8e7db950d43
JinKu <tn400xl@naver.com>
2018-08-12 21:35:52 +0900

    0812

--- Linno/js/linno.js
+++ Linno/js/linno.js
@@ -3,45 +3,8 @@ jQuery(function($){
         $(this).toggleClass('nav_btn_on');
         $('.gnb').toggleClass('nav_btn_on');
     });
-  
-      $('.com_mind_btn li').hover(function(){
-          $(this).toggleClass('mind_btn_action');
-      });
+
     
-    $('.quality_sm_img li:last').hover(function(){
-          $('.tech2').css('opacity','0');
-         $('.tech1').css('opacity','1');
-      });
-    
-     $('.quality_sm_img li:first').hover(function(){
-          $('.tech2').css('opacity','1');
-         $('.tech1').css('opacity','0');
-      });
-    
-    $('.ov_side_menu li').click(function(){
-        $(this).addClass('ov_btn_active');
-        $('.ov_side_menu li').not(this).removeClass('ov_btn_active');
-       
-    });
-    
-    $('.ov_quality').click(function(){
-        $('.ov_quality_wrap').css('opacity','1');
-    
-        $('.ov_content_wrap > div').not('.ov_quality_wrap').css('opacity','0');
-        $('.ov_wrap').css('padding-bottom',"70%");
-     });
-    
-      $('.ov_patent').click(function(){
-        $('.ov_patent_wrap').css('opacity','1');
-        $('.ov_content_wrap > div').not('.ov_patent_wrap').css('opacity','0');
-        $('.ov_wrap').css('padding-bottom',"20%");
-     });
-    
-    $('.ov_cert').click(function(){
-        $('.ov_cert_wrap').css('opacity','1');
-        $('.ov_content_wrap > div').not('.ov_cert_wrap').css('opacity','0');
-        $('.ov_wrap').css('padding-bottom',"70%");
-     });
 
    
    
@@ -58,43 +21,43 @@ jQuery(function($){
                 
     });
     
+
+
+
     
-    /*ov.btn 내용변경*/
-       $('.btn_satis').click(function(){
-        $('.mind_msg_satis').css('opacity','1');
-        
-           
-        $('.mind_msg > li').not('.mind_msg_satis').css('opacity','0');
+   $('.com_mind_wrap .com_mind_btn li').click(function(){
+       var tabIndex = $(this).index();
+       $(this).addClass('mind_btn_action').siblings().removeClass('mind_btn_action');
+
+       $('.mind_msg li').hide().eq(tabIndex).show();
+
+    });
+
+    
+
+    
+
+
+    /*ov 탭메뉴*/
+    
+     $('.ov_side_wrap .ov_side_menu li').click(function(){
+       var ovTabIndex = $(this).index();
+       $(this).addClass('ov_btn_active').siblings().removeClass('ov_btn_active');
+
+       $('.ov_content_wrap > div').hide().eq(ovTabIndex).show();
+
+     });
+
+    /*quality img 탭*/
+
+     $('.quality_sm_img li:last').hover(function(){
+          $('.tech2').css('opacity','0');
+         $('.tech1').css('opacity','1');
       });
     
-    $('.com_mind_btn li').on('click',function(){
-        var targetId = $(this).attr('data-id');
-        
-        $('.mind_msg li').hide();
-        $('#'+targetId).show();
-    })
-    
-    /*
-     $('.btn_mark').click(function(){
-        $('.mind_msg_mark').css('opacity','1');
-        $('.mind_msg > li').not('.mind_msg_mark').css('opacity','0');
+     $('.quality_sm_img li:first').hover(function(){
+          $('.tech2').css('opacity','1');
+         $('.tech1').css('opacity','0');
       });
     
-     $('.btn_sales').click(function(){
-        $('.mind_msg_sales').css('opacity','1');
-        $('.mind_msg > li').not('.mind_msg_sales').css('opacity','0');
-      });
-    
-     $('.btn_design').click(function(){
-        $('.mind_msg_design').css('opacity','1');
-        $('.mind_msg > li').not('.mind_msg_design').css('opacity','0');
-      });
-    
-     $('.btn_eco').click(function(){
-        $('.mind_msg_eco').css('opacity','1');
-        $('.mind_msg > li').not('.mind_msg_eco').css('opacity','0');
-      });
-*/
-    
-    
-})
+});
